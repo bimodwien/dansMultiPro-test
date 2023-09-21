33500a4df6c5c4022a1cd4e19589a772965a49b6
Bimo Dwien Prabowo <bimodwien2@gmail.com>
2023-09-22 00:17:22 +0700

    done wiring from backend to frontend

---
+++ server/controllers/DataController.js
@@ -0,0 +1,35 @@
+'use strict'
+// const fetch = require('node-fetch')
+
+class DataController {
+    static async fetchData(req, res) {
+        const page = `page=${req.query?.page || 1}`
+        const description = req.query?.description ? `description=${req.query.description}` : null
+        const location = req.query?.location ? `location=${req.query.location}` : null
+        const fullTime = req.query?.full_time ? `full_time=${req.query.full_time}` : null
+
+        const params = [page, description, location, fullTime].filter((item) => {
+            return !!item
+        })
+        try {
+            const response = await fetch(`https://dev6.dansmultipro.com/api/recruitment/positions.json?${params.join('&')}`)
+            const data = await response.json()
+            res.status(200).json(data)
+        } catch (error) {
+            console.log('ini error');
+        }
+    }
+
+    static async fetchDetail(req,res) {
+        try {
+            const response = await fetch(`https://dev6.dansmultipro.com/api/recruitment/positions/${req.params.id}`)
+            const data = await response.json()
+            res.status(200).json(data)
+        } catch (error) {
+            console.log('ini error');
+        }
+    }
+
+}
+
+module.exports = DataController
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,13 @@
 'use strict'
 const UserController = require('../controllers/UserController')
+const DataController = require('../controllers/DataController')
 
 const router = require('express').Router();
 
 
 router.post('/login',UserController.login)
+router.get('/data', DataController.fetchData)
+router.get('/data/:id', DataController.fetchDetail)
 
 
 module.exports = router;
